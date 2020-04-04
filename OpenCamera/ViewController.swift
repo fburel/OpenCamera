8595ade27bb8363841ce55fcdbabc375ca322773
Florian Burel <fl0@mac.com>
2020-04-04 11:20:12 +0200

    Initial Commit

---
+++ OpenCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCamera
+//
+//  Created by florian BUREL on 04/04/2020.
+//  Copyright © 2020 f10.solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
