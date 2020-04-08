cee45700e38924bf9e2047265f934b62c74d9d75
Florian Burel <fl0@mac.com>
2020-04-08 09:24:57 +0200

    first commit

--- OpenCamera/ViewController.swift
+++ OpenCamera/ViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
+import AVKit;
+
 class ViewController: UIViewController {
+    
+    private var captureSession : AVCaptureSession!;
+    private var videoPreviewLayer : AVCaptureVideoPreviewLayer!;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Create the capture session.
+        self.captureSession = nil;
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            return;
+        }
+        
+        guard let input = try? AVCaptureDeviceInput.init(device: captureDevice) else {
+            
+            // Camera permission ??
+            return;
+        }
+        
+            
+        let captureMetaDataOutput = AVCaptureMetadataOutput();
+        
+        self.captureSession  = AVCaptureSession();
+        self.captureSession.addInput(input);
+        self.captureSession.addOutput(captureMetaDataOutput);
+        
+        
+        self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession);
+        self.videoPreviewLayer.videoGravity = .resizeAspectFill;
+        self.videoPreviewLayer.frame = self.view.layer.bounds;
+        self.view.layer.addSublayer(self.videoPreviewLayer);
+        
+        self.captureSession.startRunning();
+        
+        
     }
+
 
 
 }
